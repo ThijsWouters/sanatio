2d087fd963816071f82369c12a19c7c15f010820
Thijs Wouters <thijs@morewood.be>
2015-04-15 17:26:17 +0200

    Refactor
    Pass params to class

--- lib/sanatio/validator_factory.rb
+++ lib/sanatio/validator_factory.rb
@@ -3,9 +3,9 @@ require 'sanatio/usage_error'
 
 module Sanatio
   module ValidatorFactory
-    def is(validator = nil, &validation_block)
+    def is(validator = nil, *params, &validation_block)
       if validator
-        @validator = validator.new
+        @validator = validator.new(*params)
       else
         raise UsageError.new("You need to give a block to #is.") unless block_given?
         @validator = BlockValidator.new(validation_block)
