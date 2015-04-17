9b3db29a34d5539d752cc200b592ce531429e9ad
Thijs Wouters <thijs@morewood.be>
2015-04-17 16:23:56 +0200

    Refactor
    Namespace LessThan and LessThanOrEqualTo

--- lib/sanatio/built-in/less_than.rb
+++ lib/sanatio/built-in/less_than.rb
@@ -1,21 +1,25 @@
-class LessThan
-  def initialize(value)
-    @value = value
-  end
+module Sanatio
+  module BuiltIn
+    class LessThan
+      def initialize(value)
+        @value = value
+      end
 
-  def skip?(object)
-    !object.respond_to?(:<)
-  end
+      def skip?(object)
+        !object.respond_to?(:<)
+      end
 
-  def valid?(object)
-    object < @value
-  end
+      def valid?(object)
+        object < @value
+      end
 
-  def reason
-    :greater_than_or_equal_to
-  end
+      def reason
+        :greater_than_or_equal_to
+      end
 
-  def params
-    [@value]
+      def params
+        [@value]
+      end
+    end
   end
 end
--- lib/sanatio/built-in/less_than_or_equal_to.rb
+++ lib/sanatio/built-in/less_than_or_equal_to.rb
@@ -1,21 +1,25 @@
-class LessThanOrEqualTo
-  def initialize(value)
-    @value = value
-  end
+module Sanatio
+  module BuiltIn
+    class LessThanOrEqualTo
+      def initialize(value)
+        @value = value
+      end
 
-  def skip?(object)
-    !object.respond_to?(:<=)
-  end
+      def skip?(object)
+        !object.respond_to?(:<=)
+      end
 
-  def valid?(object)
-    object <= @value
-  end
+      def valid?(object)
+        object <= @value
+      end
 
-  def reason
-    :greater_than
-  end
+      def reason
+        :greater_than
+      end
 
-  def params
-    [@value]
+      def params
+        [@value]
+      end
+    end
   end
 end
