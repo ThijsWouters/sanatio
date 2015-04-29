40c9fac64950ee99065d1517bb1b4882253463d2
Thijs Wouters <thijs@morewood.be>
2015-04-29 15:19:46 +0200

    Do not skip tests

--- test/one-time-validation/test_block_validation_with_different_types.rb
+++ test/one-time-validation/test_block_validation_with_different_types.rb
@@ -17,19 +17,13 @@ class TestBlockWithDifferentTypes < Minitest::Test
 
   def test_passes_when_symbol
     assert_valid(person_class.new(:female))
-  rescue TypeError => e
-    skip("Possible Ruby bug: #{e.inspect}")
   end
 
   def test_passes_when_coercing_string_to_symbol
     assert_valid(person_class.new("male".to_sym))
-  rescue TypeError => e
-    skip("Possible Ruby bug: #{e.inspect}")
   end
 
   def test_passes_when_bignum
     assert_valid(person_class.new(12323234534534535345345))
-  rescue TypeError => e
-    skip("Possible Ruby bug: #{e.inspect}")
   end
 end
